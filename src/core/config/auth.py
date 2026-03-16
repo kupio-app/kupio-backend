6b7f6dbd0ccf85af01ca5553b4c32367fb2b56fe
Vadim Popovych <zyzel19@gmail.com>
2026-03-16 19:49:22 +0100

    feat(config): change names for ttl fields in auth config

--- src/core/config/auth.py
+++ src/core/config/auth.py
@@ -6,5 +6,5 @@ from .base import EnvSettings
 class AuthConfig(EnvSettings, env_prefix="AUTH__"):
     jwt_secret: SecretStr = SecretStr("supersecret")
     jwt_algorithm: str = "HS256"
-    access_ttl_minutes: int = 15
-    refresh_ttl_days: int = 7
+    access_ttl: int = 15  # in minutes
+    refresh_ttl: int = 7  # in days
