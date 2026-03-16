f5111b0da7093d6ae4378ff3df4256195d188f9c
Vadim Popovych <zyzel19@gmail.com>
2026-03-16 21:37:15 +0100

    feat: connect auth router to main router

--- src/domains/router.py
+++ src/domains/router.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
+
+from .auth.router import router as auth_router
 from .users.router import router as users_router
 
 api_router = APIRouter()
 api_router.include_router(users_router, tags=["Users"], prefix="/users")
+api_router.include_router(auth_router, tags=["Auth"], prefix="/auth")
