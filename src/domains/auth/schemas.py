a329267e7eb7aeee2ac978cd62f505850af6af10
Vadim Popovych <zyzel19@gmail.com>
2026-03-17 23:22:29 +0100

    feat(auth): add fields validating for auth schemas

--- src/domains/auth/schemas.py
+++ src/domains/auth/schemas.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, ConfigDict
+import re
+
+from pydantic import BaseModel, ConfigDict, EmailStr, Field, field_validator
 import datetime
 import uuid
 
@@ -6,13 +8,22 @@ from src.domains.users.schemas import UserPrivate
 
 
 class LoginRequest(BaseModel):
-    email: str
-    password: str
+    email: EmailStr
+    password: str = Field(min_length=8, max_length=128)
     device_id: str
 
 
 class RegisterRequest(LoginRequest):
-    username: str
+    username: str = Field(min_length=3, max_length=50)
+    password: str
+
+    @field_validator("username")
+    @classmethod
+    def username_valid(cls, v: str) -> str:
+        if not re.match(r"^[a-zA-Z0-9_-]+$", v):
+            raise ValueError("Special characters are not allowed in username")
+
+        return v
 
 
 class RefreshRequest(BaseModel):
