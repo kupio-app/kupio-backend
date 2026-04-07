230e1af2f0991e3f31fc1de696ad79a9e6e34119
Vadim Popovych <zyzel19@gmail.com>
2026-04-07 22:07:13 +0200

    feat(categories): add update category endpoint

--- src/domains/categories/service.py
+++ src/domains/categories/service.py
@@ -3,7 +3,7 @@ from src.core.database.uow import UoW
 from .exceptions import CategoryNotFoundError, InvalidParentProvided
 from .repository import CategoriesRepository
 from .models import Category
-from .schemas import CategoryRequestCreate
+from .schemas import CategoryRequestCreate, CategoryRequestUpdate
 
 
 class CategoriesService:
@@ -29,14 +29,7 @@ class CategoriesService:
         )
 
     async def create_category(self, category_data: CategoryRequestCreate) -> Category:
-        parent_category: Category | None = None
-        if category_data.parent_id is not None:
-            if (
-                parent_category := await self.categories_repo.get_by_id(
-                    category_data.parent_id
-                )
-            ) is None:
-                raise InvalidParentProvided()
+        parent_category = await self._check_parent(category_data.parent_id)
 
         async with self.uow:
             return await self.categories_repo.create(
@@ -44,6 +37,21 @@ class CategoriesService:
                 icon=category_data.icon,
                 depth=0 if parent_category is None else parent_category.depth + 1,
                 parent_id=category_data.parent_id,
+            )
+
+    async def update_category(
+        self, category_data: CategoryRequestUpdate, category: Category
+    ) -> Category:
+        depth = category.depth
+        if category_data.parent_id != category.parent_id:
+            parent_category = await self._check_parent(category_data.parent_id)
+            depth = 0 if parent_category is None else parent_category.depth + 1
+
+        async with self.uow:
+            return await self.categories_repo.update(
+                category_id=category.id,
+                **category_data.model_dump(exclude_unset=True),
+                depth=depth,
             )
 
     async def get_category(self, category_id: int) -> Category:
@@ -64,3 +72,13 @@ class CategoriesService:
 
     async def get_breadcrumbs(self, category_id: int) -> list[Category]:
         return await self.categories_repo.get_breadcrumbs(category_id)
+
+    async def _check_parent(self, parent_id: int | None) -> Category | None:
+        parent_category: Category | None = None
+        if parent_id is not None:
+            if (
+                parent_category := await self.categories_repo.get_by_id(parent_id)
+            ) is None:
+                raise InvalidParentProvided()
+
+        return parent_category
