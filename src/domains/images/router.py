3c5666c36de4ff8a00653c3f0de92454f9bb19b9
Yurii Oliinyk <oliinykyurii123@gmail.com>
2026-04-09 17:22:45 +0200

    feat: add endpoints to set and delete user avatar

--- src/domains/images/router.py
+++ src/domains/images/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, UploadFile, File
 from starlette import status
 
 from src.core.database.base_model import UUID
+from src.core.dependencies import get_current_user
 from src.domains.images.dependencies import get_images_service
 from src.domains.images.schemas import (
     ListingImageResponse,
@@ -10,6 +11,8 @@ from src.domains.images.schemas import (
 from src.domains.images.service import ImageService
 from src.domains.listings.dependencies import get_owned_listing_by_id
 from src.domains.listings.models import Listing
+from src.domains.users.models import User
+from src.domains.users.schemas import UserPrivate
 
 router = APIRouter()
 
@@ -47,3 +50,20 @@ async def update_listing_images_order(
     listing: Listing = Depends(get_owned_listing_by_id),
 ):
     return await service.reorder_listing_images(listing.id, payload.image_ids)
+
+
+@router.put("/users/me/avatar", response_model=UserPrivate)
+async def set_avatar(
+    file: UploadFile = File(...),
+    service: ImageService = Depends(get_images_service),
+    current_user: User = Depends(get_current_user),
+):
+    return await service.set_user_avatar(current_user, file)
+
+
+@router.delete("/users/me/avatar", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_avatar(
+    service: ImageService = Depends(get_images_service),
+    current_user: User = Depends(get_current_user),
+) -> None:
+    await service.remove_user_avatar(current_user)
