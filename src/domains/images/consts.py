949caac88ac6260230ee23487798ec38522781a8
Yurii Oliinyk <oliinykyurii123@gmail.com>
2026-04-10 23:25:31 +0200

    refactor: use Final for constants in consts.py

--- src/domains/images/consts.py
+++ src/domains/images/consts.py
@@ -1,9 +1,12 @@
-ALLOWED_CONTENT_TYPES = ["image/jpeg", "image/png", "image/webp"]
+from typing import Final
 
-MAX_IMAGE_SIZE_BYTES = 10 * 1024 * 1024  # 10mb
 
-CONTENT_TYPE_TO_EXTENSION = {
+CONTENT_TYPE_TO_EXTENSION: Final[dict[str, str]] = {
     "image/jpeg": "jpg",
     "image/png": "png",
     "image/webp": "webp",
 }
+
+ALLOWED_CONTENT_TYPES: Final[tuple[str, ...]] = tuple(CONTENT_TYPE_TO_EXTENSION)
+
+MAX_IMAGE_SIZE_BYTES: Final[int] = 10 * 1024 * 1024  # 10mb
