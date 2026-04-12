fa4cd10da507c7f288c6a4ea3b406ef9a7faccd2
Yurii Oliinyk <oliinykyurii123@gmail.com>
2026-04-12 14:46:09 +0200

    refactor: specify consts type

--- src/domains/reports/consts.py
+++ src/domains/reports/consts.py
@@ -1,4 +1,4 @@
 from typing import Final
 
-OTHER_REPORT_REASON_SLUG: Final = "other"
-ADDITIONAL_INFO_PREVIEW_LENGTH: Final = 120
+OTHER_REPORT_REASON_SLUG: Final[str] = "other"
+ADDITIONAL_INFO_PREVIEW_LENGTH: Final[int] = 120
