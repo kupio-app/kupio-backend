b8183199a7e77f9ab5573797493c69d16545493d
Vadim Popovych <zyzel19@gmail.com>
2026-04-24 22:52:40 +0200

    chore(migrations): add migration for conv last reads

---
+++ migrations/versions/2026-04-24_unread_messages-011.py
@@ -0,0 +1,34 @@
+"""unread messages read tracking
+
+Revision ID: 010
+Revises: 009
+Create Date: 2026-04-24
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "011"
+down_revision: Union[str, Sequence[str], None] = "010"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "conversations",
+        sa.Column("buyer_last_read_at", sa.DateTime(), nullable=True),
+    )
+    op.add_column(
+        "conversations",
+        sa.Column("seller_last_read_at", sa.DateTime(), nullable=True),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("conversations", "seller_last_read_at")
+    op.drop_column("conversations", "buyer_last_read_at")
